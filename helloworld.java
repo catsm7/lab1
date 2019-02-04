b1837b1786da6945de33b476072219a7fcab1127
Catalina Sanchez Maes <cmaes@babbage.cs.nmsu.edu>
2019-02-04 12:03:32 -0700

    helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+/**
+* helloworld.java
+*  @author Catalina Sanchez-Maes
+* Janurary 30th, 2019
+* CS 371
+*/
+
+public class helloworld{//class open
+   public static void main(String[] args) {//main open
+      String str = "Hello World";
+      System.out.println(str);
+   }//end main
+}//end class
